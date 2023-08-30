712809f689ec5ca4ff1fbe8b48a5478f3564783a
kai <kai@vcnc.co.kr>
2023-08-31 01:45:45 +0900

    최조 커밋

---
+++ src/main/java/com/example/springexperiment/SpringexperimentApplication.java
@@ -0,0 +1,13 @@
+package com.example.springexperiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringexperimentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringexperimentApplication.class, args);
+    }
+
+}
