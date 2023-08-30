8c83b88108f0537af326c7df9c05a57eefca7a45
kai <kai@vcnc.co.kr>
2023-08-31 01:47:39 +0900

    테스트 컨테이너 주석 처리

--- src/test/java/com/example/springexperiment/TestSpringexperimentApplication.java
+++ src/test/java/com/example/springexperiment/TestSpringexperimentApplication.java
@@ -5,23 +5,23 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.containers.RabbitMQContainer;
+// import org.testcontainers.containers.RabbitMQContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestSpringexperimentApplication {
 
-    @Bean
-    @ServiceConnection
-    MySQLContainer<?> mysqlContainer() {
-        return new MySQLContainer<>(DockerImageName.parse("mysql:latest"));
-    }
+    // @Bean
+    // @ServiceConnection
+    // MySQLContainer<?> mysqlContainer() {
+    //     return new MySQLContainer<>(DockerImageName.parse("mysql:latest"));
+    // }
 
-    @Bean
-    @ServiceConnection
-    RabbitMQContainer rabbitContainer() {
-        return new RabbitMQContainer(DockerImageName.parse("rabbitmq:latest"));
-    }
+    // @Bean
+    // @ServiceConnection
+    // RabbitMQContainer rabbitContainer() {
+    //     return new RabbitMQContainer(DockerImageName.parse("rabbitmq:latest"));
+    // }
 
     public static void main(String[] args) {
         SpringApplication.from(SpringexperimentApplication::main).with(TestSpringexperimentApplication.class).run(args);
